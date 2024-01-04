69c14cf156adec0b63af9f84ae7ad35108a75b4a
aamirkhan237 <aamir.4u71@gmail.com>
2024-01-04 13:22:49 +0530

    Discount Fix

--- calc/perfume/models.py
+++ calc/perfume/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+
+
+class PositiveDecimalField(models.DecimalField):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.validators.append(MinValueValidator(0))
 
 
 class Product(models.Model):
@@ -13,7 +20,11 @@ class CustomerOrder(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     grams = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField(default=1)
-    discount = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
+    discount = PositiveDecimalField(
+        max_digits=5,
+        decimal_places=2,
+        default=0.0,
+    )
     row_price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def calculate_price(self):
